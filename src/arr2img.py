c4d2a801722d24b2d414eeb15910c996c343a041
wangjuan001 <275652269@qq.com>
2019-09-06 15:46:50 -0500

    update arr2img

--- src/arr2img.py
+++ src/arr2img.py
@@ -11,25 +11,38 @@ InFile = sys.argv[1] ## input .npy file
 
 HiC_max_value = 100
 arr = np.load(InFile).astype(np.float32)
+median = np.median(arr)
+arr[arr<=median] = 0
+#arr = np.triu(arr)
+x, y = np.where(arr!=0)
+#arr[y, x] = arr[x, y] ## if it's predicted from intra chromosome
+arr = arr * (1/arr[x,y].min())
+#vmax = np.percentile(arr[x,y], 95)
 #print(arr)
-arr = ma.log(arr)+2
-arr = arr.filled(0)
-MaxValue= np.mean(arr)
+arr[arr<=1] = 1
+arr = np.log(arr)
+#arr = arr.filled(0)
+#MaxValue= np.mean(arr)
 
-arr = arr-MaxValue/MaxValue
+#arr = arr-MaxValue/MaxValue
 #print(arr)
 #arr = np.ma.masked_where(arr < 0.0001, arr)
 
 #arr = arr.astype('uint8')
 cmap = plt.get_cmap('Reds')
+
+plt.imshow(arr, interpolation='none', cmap=cmap, vmax=0.3)
+plt.colorbar()
+plt.savefig('test.png', dpi=300, bbox_inches='tight')
+
 #cmap.set_bad(color = 'white')
 
-rgba_arr = cmap(arr)
+#rgba_arr = cmap(arr)
 #print(rgba_arr)
-rgb_arr = np.delete(rgba_arr,3,2)
+#rgb_arr = np.delete(rgba_arr,3,2)
 #print(rgb_arr.shape)
 #rgb_arr =rgb_arr.astype('uint8')
-plt.imsave(InFile+'.jpg',rgb_arr)
+#plt.imsave(InFile+'.jpg',rgb_arr)
 
 #new_im = Image.fromarray(rgb_arr)#.convert('RGB')
 
