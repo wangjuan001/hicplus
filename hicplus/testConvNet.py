e5f114e72eed157c019d1b8670610a8ad63ee352
wangjuan001 <275652269@qq.com>
2019-09-18 11:32:07 -0500

    clean

--- hicplus/testConvNet.py
+++ hicplus/testConvNet.py
@@ -1,4 +1,4 @@
-# Author: Yan Zhang  
+# Author: Yan Zhang
 # Email: zhangyan.cse (@) gmail.com
 
 import sys
@@ -32,7 +32,7 @@ HiC_max_value = 100
 
 
 
-# This block is the actual training data used in the training. The training data is too large to put on Github, so only toy data is used. 
+# This block is the actual training data used in the training. The training data is too large to put on Github, so only toy data is used.
 # cell = "GM12878_replicate"
 # chrN_range1 = '1_8'
 # chrN_range = '1_8'
@@ -50,14 +50,14 @@ low_resolution_samples = np.minimum(HiC_max_value, low_resolution_samples)
 
 batch_size = low_resolution_samples.shape[0]
 
-# Reshape the high-quality Hi-C sample as the target value of the training. 
+# Reshape the high-quality Hi-C sample as the target value of the training.
 sample_size = low_resolution_samples.shape[-1]
 padding = conv2d1_filters_size + conv2d2_filters_size + conv2d3_filters_size - 3
 half_padding = padding / 2
 output_length = sample_size - padding
 
 
-print low_resolution_samples.shape
+print(low_resolution_samples.shape)
 
 lowres_set = data.TensorDataset(torch.from_numpy(low_resolution_samples), torch.from_numpy(np.zeros(low_resolution_samples.shape[0])))
 lowres_loader = torch.utils.data.DataLoader(lowres_set, batch_size=batch_size, shuffle=False)
@@ -90,34 +90,28 @@ running_loss_validate = 0.0
 reg_loss = 0.0
 
 
-for i, (v1, v2) in enumerate(zip(lowres_loader, hires_loader)):    
+for i, (v1, v2) in enumerate(zip(lowres_loader, hires_loader)):
     _lowRes, _ = v1
     _highRes, _ = v2
-    
+
 
     _lowRes = Variable(_lowRes)
     _highRes = Variable(_highRes)
 
-    
+
     if use_gpu:
         _lowRes = _lowRes.cuda()
         _highRes = _highRes.cuda()
     y_prediction = Net(_lowRes)
     if (not production):
-        loss = _loss(y_prediction, _highRes) 
+        loss = _loss(y_prediction, _highRes)
 
 
     running_loss += loss.data[0]
-    
-print '-------', i, running_loss, strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+print('-------', i, running_loss, strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 
 y_prediction = y_prediction.data.cpu().numpy()
 
-print y_prediction.shape
-
-
-
-
-
-
+print(y_prediction.shape)
