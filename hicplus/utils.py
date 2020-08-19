8efe82caca5af0470f079c1ceaa456a7c0a7bfce
wangjuan001 <275652269@qq.com>
2020-08-19 17:35:19 -0500

    Update utils.py

--- hicplus/utils.py
+++ hicplus/utils.py
@@ -7,7 +7,72 @@ from scipy.sparse import csr_matrix, coo_matrix, vstack, hstack
 from scipy import sparse
 import numpy as np
 
-chrs_length = [0,249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,135006516,133851895,115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,48129895,51304566]
+def readcstr(f):
+    buf = ""
+    while True:
+        b = f.read(1)
+        b = b.decode('utf-8', 'backslashreplace')
+        if b is None or b == '\0':
+            return str(buf)
+        else:
+            buf = buf + b
+
+def read_hic_header(hicfile):
+
+    if not os.path.exists(hicfile):
+        return None  # probably a cool URI
+
+    req = open(hicfile, 'rb')
+    magic_string = struct.unpack('<3s', req.read(3))[0]
+    req.read(1)
+    if (magic_string != b"HIC"):
+        return None  # this is not a valid .hic file
+
+    info = {}
+    version = struct.unpack('<i', req.read(4))[0]
+    info['version'] = str(version)
+
+    masterindex = struct.unpack('<q', req.read(8))[0]
+    info['Master index'] = str(masterindex)
+
+    genome = ""
+    c = req.read(1).decode("utf-8")
+    while (c != '\0'):
+        genome += c
+        c = req.read(1).decode("utf-8")
+    info['Genome ID'] = str(genome)
+
+    nattributes = struct.unpack('<i', req.read(4))[0]
+    attrs = {}
+    for i in range(nattributes):
+        key = readcstr(req)
+        value = readcstr(req)
+        attrs[key] = value
+    info['Attributes'] = attrs
+
+    nChrs = struct.unpack('<i', req.read(4))[0]
+    chromsizes = {}
+    for i in range(nChrs):
+        name = readcstr(req)
+        length = struct.unpack('<i', req.read(4))[0]
+        if name != 'ALL':
+            chromsizes[name] = length
+
+    info['chromsizes'] = chromsizes
+
+    info['Base pair-delimited resolutions'] = []
+    nBpRes = struct.unpack('<i', req.read(4))[0]
+    for i in range(nBpRes):
+        res = struct.unpack('<i', req.read(4))[0]
+        info['Base pair-delimited resolutions'].append(res)
+
+    info['Fragment-delimited resolutions'] = []
+    nFrag = struct.unpack('<i', req.read(4))[0]
+    for i in range(nFrag):
+        res = struct.unpack('<i', req.read(4))[0]
+        info['Fragment-delimited resolutions'].append(res)
+
+    return info
 
 def matrix_extract(chrN1, binsize, hicfile):
 
@@ -19,9 +84,9 @@ def matrix_extract(chrN1, binsize, hicfile):
     #print(N)
     M = csr_matrix((value, (row,col)), shape=(N,N))
     M = csr_matrix.todense(M)
-    M = np.array(M)
-    x, y = np.where(M!=0)
-    M[y, x] = M[x, y]
+    # M = np.array(M)
+    # x, y = np.where(M!=0)
+    # M[y, x] = M[x, y]
     #rowix = range(start1//binsize, end1//binsize+1)
     #colix = range(start2//binsize, end2//binsize+1)
     #print(rowix,colix)
@@ -29,32 +94,33 @@ def matrix_extract(chrN1, binsize, hicfile):
     #N = M.shape[1]
     return(M)
 
-def frag_matrix_extract(hicfile, chrN1, chrN2, binsize, start1, start2, lastend1, lastend2, shiftsize,Step):
-
-    end1=start1+Step + shiftsize
-    end2=start2+Step + shiftsize
-    #if end1 > lastend1:
-    #    end1 = lastend1
-    #if end2 > lastend2:
-    #    end2 = lastend2
-    result = straw.straw('NONE', hicfile, str(chrN1),str(chrN2),'BP',binsize)
-    row = [r//binsize for r in result[0]]
-    col = [c//binsize for c in result[1]]
-    value = result[2]
-
-    N = max(chrs_length[chrN2]//binsize+Step//binsize, chrs_length[chrN1]//binsize+Step// binsize) +1
-    #N = max(max(row)+1, max(col) + 1)
-    #print(N)
-    M = csr_matrix((value, (row,col)), shape=(N,N))
-    M = csr_matrix.todense(M)
-    rowix = range(start1//binsize, end1//binsize+1)
-    colix = range(start2//binsize, end2//binsize+1)
-    #print(rowix,colix)
-    M = M[np.ix_(rowix, colix)]
-    N = M.shape[1]
-    return(M,N)
-
 def divide(HiCmatrix):
+    subImage_size = 40
+    step = 5
+
+    #chrN = 21  ##need to change.
+
+    total_loci = HiCmatrix.shape[0]
+    #print(HiCmatrix.shape)
+    for i in range(0, total_loci, step):
+        result = []
+        index = []
+        for j in range(0, total_loci,step ):
+            if (i + subImage_size >= total_loci or j + subImage_size >= total_loci):
+                continue
+            subImage = HiCmatrix[i:i + subImage_size, j:j + subImage_size]
+
+            result.append([subImage, ])
+            tag = 'test'
+            index.append((tag, i, j))
+        result = np.array(result)
+    #print(result.shape)
+    #result = result.astype(np.double)
+        index = np.array(index)
+        yield result, index
+
+
+def train_divide(HiCmatrix):
     subImage_size = 40
     step = 25
     result = []
@@ -65,7 +131,7 @@ def divide(HiCmatrix):
     #print(HiCmatrix.shape)
     for i in range(0, total_loci, step):
         for j in range(0, total_loci, ):
-            if (i + subImage_size >= total_loci or j + subImage_size >= total_loci):
+            if (abs(i-j)>201 or i + subImage_size >= total_loci or j + subImage_size >= total_loci):
                 continue
             subImage = HiCmatrix[i:i + subImage_size, j:j + subImage_size]
 
@@ -90,3 +156,4 @@ def genDownsample(original_sample, rate):
 
 if __name__ == "__main__":
     main()
+
